74463338c42bc7f7039fdce900972a663524bef5
twentyfive21 <tina.winnn@gmail.com>
2023-09-08 21:54:25 -0400

    added routes to app and two components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-    <Homepage />
+    <BrowserRouter>
+    <Header />
+
+    <Routes>
+    <Route path='/' element={<Homepage />} />
+    </Routes>
+
+    </BrowserRouter>
     </>
   )
 }
