167593560796867cbf66854af70d39dee0343551
twentyfive21 <tina.winnn@gmail.com>
2023-09-09 14:02:51 -0400

    banner css for top part finsihed fir desktop

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage/Homepage'
 import Header from './components/Header/Header'
 
+
 function App() {
 
   return (
