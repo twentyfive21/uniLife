9db6ec2eb98ddfb5124a3a4e673fa68b13828e19
twentyfive21 <tina.winnn@gmail.com>
2023-09-10 20:06:46 -0400

    css styling cities section before mapping out data

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -2,6 +2,7 @@ import {React, useEffect} from 'react'
 import './Homepage.css'
 import Banner from '../../components/Banner/Banner'
 import select from '../../assets/header/select.png'
+import city from '../../assets/middle/city.png'
 
 function Homepage() {
   
@@ -20,17 +21,31 @@ useEffect(()=>{
 }, [])
 
   return (
-    <div className='homepage-box'>
+    <div>
+    <section className='header-box'>
     <Banner />
     <div className='form-container'>
     <form>
-      <img src={select} className='arrow' alt='arrow'/>
+      <img src={select} className='arrow'alt='arrow'/>
       <select>
         <option>Search by City</option>
       </select>
       <button>Find Homes</button>
     </form>
     </div>
+    </section>
+    <section className='cities-section'>
+    <h2>Student accommodations in our top cities </h2>
+      <section className='cities-container'>
+      <div className='city-card'>
+        <div className='city-details'>
+        <h2>Leeds</h2>
+        <p>2 properties</p>
+        </div>
+      <img src={city} alt='city' />
+      </div>
+      </section>
+    </section>
     </div>
   )
 }
