4891eee06bf46887440f45622dca3c73e9782957
twentyfive21 <tina.winnn@gmail.com>
2023-09-15 15:50:56 -0400

    data for top compare and added image

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,30 @@
+import search from '../assets/middle/earth.png'
+import compare from '../assets/middle/compare.png'
+import bills from '../assets/middle/bills.png'
 const data = [
+    {
+       info: 
+       [
+        {
+           id: 0,
+           img: search,
+           title: 'Search',
+           text:'Find your dream home in the perfect area near your university.' 
+        },
+        {
+           id: 1,
+           img: compare,
+           title: 'Compare',
+           text:'Compare student accommodation to find the right home for you.'  
+        },
+        {
+           id: 2,
+           img: bills,
+           title:'Bills Included',
+           text:'Bills are included in all rent prices. No hidden fees.'  
+        }
+       ]
+    },
 
 ]
 export default data 
