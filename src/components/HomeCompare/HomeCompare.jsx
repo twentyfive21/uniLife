14da071e369e0dc5d1bbef1a6f3114d181a73fd4
twentyfive21 <tina.winnn@gmail.com>
2023-09-15 15:52:08 -0400

    imported data file and mapped out static data

--- src/components/HomeCompare/HomeCompare.jsx
+++ src/components/HomeCompare/HomeCompare.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './HomeCompare.css'
+import data from '../../utils/data.js'
 
 function HomeCompare() {
   return (
-    <div>HomeCompare</div>
+    <div className='compare-container'>
+      <section className='top-compare-box'>
+        <h2>Compare all inclusive student homes.</h2>
+        <section className='top-compare'>
+        {data[0].info.map(item => (
+          <div key={item.id} className='compare-card'>
+            <img src={item.img} />
+            <div>
+              <h3>{item.title}</h3>
+              <p>{item.text}</p>
+            </div>
+          </div>
+        ))}
+        </section>
+      </section>
+      <section>
+        
+      </section>
+    </div>
   )
 }
 
 export default HomeCompare
+{/* <p>{data[0].info[0].title}</p> */}
