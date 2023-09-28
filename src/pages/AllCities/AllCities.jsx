87be7f117a04db8d4c86b872d1fba346bafd6fd5
twentyfive21 <tina.winnn@gmail.com>
2023-09-28 16:17:16 -0400

    banner object passed for all cities page

--- src/pages/AllCities/AllCities.jsx
+++ src/pages/AllCities/AllCities.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './AllCities.css'
+import Banner from '../../components/Banner/Banner'
+import data from '../../utils/data'
 
 function AllCities() {
   return (
     <div>
-    AllCities
+    <Banner data={data[2].info[1]}/>
     </div>
   )
 }
