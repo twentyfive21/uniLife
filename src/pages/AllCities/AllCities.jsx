3734a5e4884c1b20122c2cd9b99174369aee7b07
twentyfive21 <tina.winnn@gmail.com>
2023-09-28 16:40:36 -0400

    created context to make cities data avaiable everywhere without fetching multiple times

--- src/pages/AllCities/AllCities.jsx
+++ src/pages/AllCities/AllCities.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import './AllCities.css'
 import Banner from '../../components/Banner/Banner'
 import data from '../../utils/data'
+import { CityContext } from '../../contexts/CityContext'
 
 function AllCities() {
+  const {error, cities} = useContext(CityContext) 
   return (
     <div>
     <Banner data={data[2].info[1]}/>
