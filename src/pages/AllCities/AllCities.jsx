119f6a03363fb8f9c13ff9c4e5d3529eed48a8cf
twentyfive21 <tina.winnn@gmail.com>
2023-09-29 18:15:12 -0400

    mapped out cities while api is down

--- src/pages/AllCities/AllCities.jsx
+++ src/pages/AllCities/AllCities.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import './AllCities.css'
 import Banner from '../../components/Banner/Banner'
 import data from '../../utils/data'
@@ -7,8 +7,20 @@ import { CityContext } from '../../contexts/CityContext'
 function AllCities() {
   const {error, cities} = useContext(CityContext) 
   return (
-    <div>
+    <div className='allcities-container'>
     <Banner data={data[2].info[1]}/>
+    <h1>Search by City</h1>
+    <section>
+      {
+        cities.map(city => 
+        <div key={city._id} className='allcities-selection'>
+          <p>{city.name}</p>
+        </div>)
+      }
+    </section>
+    {
+      error && <h2>The site is currently down please check back later! </h2>
+    }
     </div>
   )
 }
