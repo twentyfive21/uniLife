09c558538e56e0ccc0a0aef4a69e524d0e4be644
twentyfive21 <tina.winnn@gmail.com>
2023-09-30 21:27:51 -0400

    created cities about page and waiting to get description from API

---
+++ src/components/CitiesAbout/CitiesAbout.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './CitiesAbout.css'
+
+function CitiesAbout() {
+  return (
+    <div>CitiesAbout</div>
+  )
+}
+
+export default CitiesAbout
--- src/pages/CityDetails/CityDetails.jsx
+++ src/pages/CityDetails/CityDetails.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import './CityDetails.css'
 import Banner from '../../components/Banner/Banner'
 import data from '../../utils/data'
+import CitiesSelection from '../../components/CitiesSelection/CitiesSelection'
+import CitiesAbout from '../../components/CitiesAbout/CitiesAbout'
 
 function CityDetails() {
   return (
     <div>
     <Banner data={data[2].info[2]}/>
+    <CitiesSelection />
+    <CitiesAbout />
     </div>
   )
 }
