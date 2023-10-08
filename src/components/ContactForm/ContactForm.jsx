eef4129346c3f31253ccfb4eccdac692e2d5aa84
twentyfive21 <tina.winnn@gmail.com>
2023-10-07 22:25:26 -0400

    created contact form for modal

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import './ContactForm.css'
+
+function ContactForm() {
+  return (
+    <form className='contact-container'>
+        <p>hello</p>
+    </form>
+  )
+}
+
+export default ContactForm
