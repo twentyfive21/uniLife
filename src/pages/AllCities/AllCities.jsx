dd7a091e2df8fea91942c92594bf3ef44264406d
twentyfive21 <tina.winnn@gmail.com>
2023-10-07 21:32:27 -0400

    navigated onclick to move city id to details page

--- src/pages/AllCities/AllCities.jsx
+++ src/pages/AllCities/AllCities.jsx
@@ -3,18 +3,26 @@ import './AllCities.css'
 import Banner from '../../components/Banner/Banner'
 import data from '../../utils/data'
 import { CityContext } from '../../contexts/CityContext'
+import { useNavigate } from 'react-router-dom'
 
 function AllCities() {
-  const {error, cities} = useContext(CityContext) 
+
+  const navigate = useNavigate();
+  const {error, cities} = useContext(CityContext);
+
+  const handleChosenCity = (city) => {
+    navigate(`/citydetails/${city}`)
+  }
+
   return (
     <div className='allcities-container'>
     <Banner data={data[2].info[1]}/>
     <h1>Search by City</h1>
     <section>
       {
-        cities.map(city => 
-        <button key={city._id} className='allcities-selection'>
-          <p>{city.name}</p>
+        cities?.map(city => 
+        <button onClick={()=>handleChosenCity(city?._id)} key={city?._id} className='allcities-selection'>
+          <p>{city?.name}</p>
         </button>)
       }
     </section>
